56cfb72df5a1cd3d4f0ec724e6d7725f147d223e
ErjonJanuzaj <erjoni1619@icloud.com>
2023-12-06 00:30:26 +0100

    added login page

---
+++ html/javascript/login.js
@@ -0,0 +1,21 @@
+document.getElementById("loginButton").addEventListener("click", function() {
+    login();
+  });
+  
+  function login() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+  
+    // You can add your authentication logic here
+    // For simplicity, let's check if both fields are non-empty
+    if (username == "dardan" && password == "1234") {
+      alert("Login successful!");
+    }  
+    else if(username ==='' || password ==='') {
+            alert('Username and password are required.');
+            return false;
+    }
+    else if(username !== "dardan" || password !== "1234"){
+      alert("Wrong credentials");
+    }
+  }
